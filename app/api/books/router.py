1213407cec5f8fb51feb225b41d390042aec49a4
tuanvuPresent <55792941+tuanvuPresent@users.noreply.github.com>
2022-08-12 18:34:35 +0700

    Update router.py

--- app/api/books/router.py
+++ app/api/books/router.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter
+from fastapi_restful.cbv import cbv
 
 from app.api.books.schema import BookSchema, BookSchemaRequest, BookSchemaResponse
 from app.api.books.service import BookService
@@ -11,35 +12,34 @@ from app.core.schema import DataResponse
 book_router = APIRouter()
 
 
-@book_router.post("/books/", response_model=DataResponse[BookSchemaResponse])
-async def create_books(data: BookSchemaRequest):
-    return DataResponse().success_response(BookService().create(data))
+@cbv(book_router)
+class MyApi:
 
+    @book_router.post("/books/", response_model=DataResponse[BookSchemaResponse])
+    async def create_books(self, data: BookSchemaRequest):
+        return DataResponse().success_response(BookService().create(data))
 
-@book_router.get("/books/", response_model=DataResponse[List[BookSchema]])
-async def get_books(limit: int, offset: int = 0):
-    return DataResponse().success_response(BookService().list(limit, offset))
+    @book_router.get("/books/", response_model=DataResponse[List[BookSchema]])
+    async def get_books(self, limit: int, offset: int = 0):
+        return DataResponse().success_response(BookService().list(limit, offset))
 
+    @book_router.get("/books/{id}", response_model=DataResponse[BookSchema])
+    async def retrieve_book(self, id: str):
+        book = BookService().get(id)
+        if book is None:
+            raise CustomException(**ErrorMessage.NOT_FOUND)
+        return DataResponse().success_response(book)
 
-@book_router.get("/books/{id}", response_model=DataResponse[BookSchema])
-async def retrieve_book(id: str):
-    book = BookService().get(id)
-    if book is None:
-        raise CustomException(**ErrorMessage.NOT_FOUND)
-    return DataResponse().success_response(book)
+    @book_router.put("/books/{id}", response_model=DataResponse[BookSchemaResponse])
+    async def update_book(self, id: str, data: BookSchemaRequest):
+        book = BookService().update(id, data)
+        if book is None:
+            raise CustomException(**ErrorMessage.NOT_FOUND)
+        return DataResponse().success_response(book)
 
-
-@book_router.put("/books/{id}", response_model=DataResponse[BookSchemaResponse])
-async def update_book(id: str, data: BookSchemaRequest):
-    book = BookService().update(id, data)
-    if book is None:
-        raise CustomException(**ErrorMessage.NOT_FOUND)
-    return DataResponse().success_response(book)
-
-
-@book_router.delete("/books/{id}", response_model=DataResponse[BookSchemaResponse])
-async def delete_book(id: str):
-    book = BookService().delete(id)
-    if book is None:
-        raise CustomException(**ErrorMessage.NOT_FOUND)
-    return DataResponse().success_response(book)
+    @book_router.delete("/books/{id}", response_model=DataResponse[BookSchemaResponse])
+    async def delete_book(self, id: str):
+        book = BookService().delete(id)
+        if book is None:
+            raise CustomException(**ErrorMessage.NOT_FOUND)
+        return DataResponse().success_response(book)
