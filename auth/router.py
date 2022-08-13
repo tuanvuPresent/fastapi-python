fc6accb0a12645415165e50a718b681494817de4
tuanvuPresent <55792941+tuanvuPresent@users.noreply.github.com>
2022-08-13 10:57:51 +0700

    Add files via upload

---
+++ auth/router.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from fastapi import APIRouter
+from fastapi_restful.cbv import cbv
+
+from app.api.auth.schema import LoginRequestResponse, LoginRequestSchema
+from app.core.jwt_handle import jwt_payload_handler, jwt_encode_handler
+from app.core.schema import DataResponse
+
+auth_router = APIRouter()
+
+
+@cbv(auth_router)
+class AuthApiView:
+
+    @auth_router.post("/login", response_model=DataResponse[LoginRequestResponse])
+    async def login(self, data: LoginRequestSchema):
+        # extra login here
+        timestamp = datetime.now().timestamp()
+        payload = jwt_payload_handler({'id': data.username}, timestamp)
+        return DataResponse().success_response({
+            'token': jwt_encode_handler(payload)
+        })
