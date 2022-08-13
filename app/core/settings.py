00e0f95a3fd8e222ca954c3fa21715a28445c79a
tuanvuPresent <55792941+tuanvuPresent@users.noreply.github.com>
2022-08-13 11:01:05 +0700

    Update settings.py

--- app/core/settings.py
+++ app/core/settings.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     DATABASE_MYSQL_URL: str
     BACKEND_CORS_ORIGINS: str
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = 'HS256'
+    JWT_EXPIRATION_DELTA: int
 
     @validator("BACKEND_CORS_ORIGINS")
     def assemble_cors_origins(cls, v: str):
